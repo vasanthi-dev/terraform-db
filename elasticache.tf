cbbb1d2cdaa5c85918e92a4186e01c51bde2d69d
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-22 10:40:41 -0400

    Mongodb and RabbitMQ instances

---
+++ elasticache.tf
@@ -0,0 +1,44 @@
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "redis-${var.ENV}"
+  engine               = "redis"
+  node_type            = "cache.t3.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis6.x"
+  engine_version       = "6.2 "
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
+  security_group_ids = [aws_security_group.redis.id]
+}
+
+resource "aws_elasticache_subnet_group" "subnet-group" {
+  name       = "redis-${var.ENV}"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
+}
+
+resource "aws_security_group" "redis" {
+  name        = "sg_redis_${var.ENV}"
+  description = "sg_redis_${var.ENV}"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description      = "APP"
+    from_port        = 6379
+    to_port          = 6379
+    protocol         = "tcp"
+    cidr_blocks      = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_CIDR
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "sg_redis_${var.ENV}"
+  }
+}
+
+
