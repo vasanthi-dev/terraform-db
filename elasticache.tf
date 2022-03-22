3e5ca7627f81d44928ff34674a4ff1c15b383f38
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-22 10:46:32 -0400

    Redis in amazon service elasticache setup

--- elasticache.tf
+++ elasticache.tf
@@ -3,8 +3,7 @@ resource "aws_elasticache_cluster" "redis" {
   engine               = "redis"
   node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis6.x"
-  engine_version       = "6.2 "
+  engine_version       = "6.x"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
   security_group_ids = [aws_security_group.redis.id]
