ee9a9e790274c41c2b60e245a456ca022731f770
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-22 06:23:35 -0400

    Mongodb and RabbitMQ instances

--- module/ec2/ec2.tf
+++ module/ec2/ec2.tf
@@ -9,3 +9,9 @@ resource "aws_spot_instance_request" "db" {
   wait_for_fulfillment = true
   vpc_security_group_ids = [aws_security_group.sg.id]
 }
+
+resource "aws_ec2_tag" "spot-instances" {
+  resource_id = aws_spot_instance_request.db.*.spot_instance_id
+  key         = "Name"
+  value       = "${var.DB_COMPONENT}-${var.ENV}"
+}
