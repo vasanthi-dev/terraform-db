c64e9ec16ea96b0ebe7bb416bf4cc67903d36b47
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-23 05:10:58 -0400

    Elasticache service in amazon

--- elasticache.tf
+++ elasticache.tf
@@ -44,3 +44,11 @@ output "test" {
   value = aws_elasticache_cluster.redis
 }
 
+
+resource "aws_route53_record" "redis" {
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONEID
+  name    = "redis-${var.ENV}.roboshop.internal"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_elasticache_cluster.redis.cache_nodes[0].address]
+}
